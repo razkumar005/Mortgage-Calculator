593171e28f330758c0d8e9e4f2670b8927d52dfa
rajkumar <thotla.rajkumar@gmail.com>
2026-08-28 10:57:39 +0530

    refracted to methods

--- src/main/java/com/rajkumar/calculator/App.java
+++ src/main/java/com/rajkumar/calculator/App.java
@@ -9,144 +9,133 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class App {
+
+
     public static void main(String[] args) {
         int principle;
-        float interest;
         float rate;
-        long duration;
-        double balanceAmount;
         final String USER_NAME;
         int tenure;
-        double[] emipayments;
-        double emi;
-        boolean isAllValuesAreNonNegative;
-        boolean hasHighIncome = false;
-        boolean hasGoodCredit = false;
-        boolean hasCriminalRecord;
         int creditScore;
         int income;
         boolean isEligible;
-        int count = 0;
+        String criminalRecord;
+        int choice;
+
+        USER_NAME = readStrings("Please enter your name :");
+        income = (int) readNumber("Enter your income :", 1000, 1000000);
+        principle = (int) readNumber("Principle : ", 1000, 10_00_00_000);
+        rate = (float) readNumber("Annual Rate of interest :", 0, 30);
+        tenure = (int) readNumber("Enter the Tenure in months :", 1, 100);
+        creditScore = (int) readNumber("Enter credit score :", 500, 800);
+        criminalRecord = readStrings("Has any criminal record : ");
+        String date = readStrings("Enter disbursement date (yyyy-MM-dd): ");
+        choice = (int) readNumber("""
+                Mortgage Calculator
+                1. Calculate EMI\s
+                2. Pending EMI's
+                3. Show Balance\s
+                4. Exit
+                """, 1, 4);
+        isEligible = !criminalRecord.equalsIgnoreCase("yes")
+                && (income > 100000 || creditScore > 600);
+        if (isEligible)
+            result(choice, principle, rate, tenure, date);
+        else
+            System.out.println(USER_NAME + "You are not eligible \nThank you \n Better luck next time ");
+
+        typeOfCustomer(creditScore);
+    }
+
+    public static void typeOfCustomer(int creditScore) {
+        int category = creditScore / 50;
+        switch (category) {
+            case (15):
+                System.out.print("\nExcellent credit score ");
+                break;
+            case 14:
+                System.out.print("\nGood credit score ");
+                break;
+            case 13:
+                System.out.print("\nAverage credit score");
+                break;
+            case 12:
+                System.out.print("\nneeds to improve creditor ");
+                break;
+            default:
+                System.out.println("\nPoor credit score ");
+
+        }
+    }
+
+    public static double calculateMortgage(int principle, float rate, int tenure) {
+        rate = rate / 1200;
+        double emi = principle * rate * Math.pow(1 + rate, tenure) / (Math.pow(1 + rate, tenure) - 1);
+        emi = Math.round(emi * 100.0) / 100.0;
+        return emi;
+    }
+
+    public static double readNumber(String prompt, int min, int max) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter your name :");
-        USER_NAME = scanner.next();
-
+        double value;
         while (true) {
-            System.out.print("Enter your income :");
-            income = scanner.nextInt();
-            if (income > 0) {
-                hasHighIncome = income >= 10_00_000;
+            System.out.print(prompt);
+            value = scanner.nextInt();
+            if (value >= min && value <= max) {
                 break;
             } else {
-                System.out.println("Income should be greater than 0");
+                System.out.println(prompt + " should be between " + min + " and " + max);
             }
         }
+        return value;
+    }
 
+    public static String readStrings(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        String value;
         while (true) {
-            System.out.print("Enter the credit score : ");
-            creditScore = scanner.nextInt();
-            if (creditScore >= 600 && creditScore < 800) {
-                hasGoodCredit = true;
+            System.out.print(prompt);
+            value = scanner.next();
+            if (!value.isEmpty()) {
                 break;
             } else {
-                System.out.println("Credit score should be between 600 and 800");
+                System.out.println(prompt + "Please enter valid input");
             }
         }
+        return value;
+    }
 
+    public static void result(int choice, int principle, float rate, int tenure, String date) {
+        double emi = calculateMortgage(principle, rate, tenure);
+        long months = elapsedMonths(date);
+        int pendingEmis = (int) (tenure - months);
+        switch (choice) {
+            case 1:
+                System.out.print("\nYour monthly emi is " + NumberFormat.getCurrencyInstance().format(emi));
+                break;
+            case 2:
+                System.out.print("\nEMI's pending " + pendingEmis);
+                break;
+            case 3:
+                System.out.print("Your total balanceAmount to be paid is "
+                        + NumberFormat.getCurrencyInstance().format(calculateBalanceAmount(principle, months, rate, emi)));
+                break;
+            case 4:
+                System.out.print("Exiting ");
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
+    }
 
-        System.out.print("Has any criminal record :");
-        hasCriminalRecord = scanner.nextBoolean();
-        isEligible = (!hasCriminalRecord && (hasGoodCredit || hasHighIncome));
-        if (isEligible) {
+    public static double calculateBalanceAmount(int principle, long paidMonths, float rate, double emi) {
+        return principle * Math.pow(1 + rate, paidMonths) - emi * (Math.pow(1 + rate, paidMonths) - 1) / rate;
 
-            System.out.print("Principle : ");
-            principle = scanner.nextInt();
+    }
 
-            System.out.print("Annual Rate of interest :");
-            rate = scanner.nextFloat();
-            int categeory = creditScore / 50;
-            switch (categeory) {
-                case (15):
-                    System.out.println("Excellent credit score ");
-                    break;
-                case 14:
-                    System.out.println("Good credit score ");
-                    break;
-                case 13:
-                    System.out.println("Average credit score");
-                    break;
-                case 12:
-                    System.out.println("needs to improve creditor ");
-                    break;
-                default:
-                    System.out.println("Poor credit score ");
-
-            }
-            rate = (float) rate / 1200;
-
-            System.out.print("Enter the Tenure in months :");
-            tenure = scanner.nextInt();
-
-            System.out.print("Enter disbursement date (yyyy-MM-dd): ");
-            String date = scanner.next();
-            LocalDate disburseDate = LocalDate.parse(date);
-
-
-            LocalDate today = LocalDate.now();
-            duration = ChronoUnit.DAYS.between(disburseDate, today);
-            long elapsedMonths = ChronoUnit.MONTHS.between(disburseDate, today);
-            long pendingEmis = tenure - elapsedMonths;
-            isAllValuesAreNonNegative = principle > 0 && rate > 0 && (tenure > 0);
-
-            if (isAllValuesAreNonNegative) {
-                if (!(pendingEmis <= 0)) {
-                    emi = principle * rate * Math.pow(1 + rate, tenure) / (Math.pow(1 + rate, tenure) - 1);
-                    emi = Math.round(emi * 100.0) / 100.0;
-
-                    balanceAmount = principle * Math.pow(1 + rate, elapsedMonths) - emi * (Math.pow(1 + rate, elapsedMonths) - 1) / rate;
-                    System.out.println();
-                    emipayments = new double[tenure];
-
-                    for (int i = 0; i < elapsedMonths; i++) {
-                        emipayments[i] = emi;
-                        count++;
-                    }
-
-                    System.out.println("\nMortgage Calculator");
-                    System.out.println("1. Calculate EMI");
-                    System.out.println("2. Pending EMI's");
-                    System.out.println("3. Show Balance");
-                    System.out.println("4. Exit");
-
-                    System.out.print("Choose an option: ");
-                    int choice = scanner.nextInt();
-
-                    switch (choice) {
-                        case 1:
-                            System.out.print("\nYour monthly emi is " + NumberFormat.getCurrencyInstance().format(emi));
-                            break;
-                        case 2:
-                            System.out.print("\nEMI's pending " + pendingEmis);
-                            break;
-                        case 3:
-                            System.out.print("Your total balanceAmount to be paid is "
-                                    + NumberFormat.getCurrencyInstance().format(balanceAmount));
-                            break;
-                        case 4:
-                            System.out.print("Exiting ");
-                            break;
-                        default:
-                            System.out.println("Invalid input");
-                    }
-
-                    scanner.close();
-                    System.out.println();
-                    System.out.println(USER_NAME.concat(" ") + "You have paid till now " + count + " emi's");
-                } else
-                    System.out.println("Your loan is completed");
-            } else
-                System.out.println("Please enter positive values");
-        } else
-            System.out.println("Thank you Better luck next time");
+    public static long elapsedMonths(String date) {
+        LocalDate disburseDate = LocalDate.parse(date);
+        LocalDate today = LocalDate.now();
+        return ChronoUnit.MONTHS.between(disburseDate, today);
     }
 }
